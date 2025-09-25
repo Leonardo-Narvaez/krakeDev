c5cf995e48e73a566e3fcfd74535abf0213473f4
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2025-09-25 00:27:47 -0500

    se funciones para recuperar texto, imagen y cambio de texto en caja; ademas de la separacion de las funciones en el archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,20 +3,8 @@ saludar=function(){
     let apellido=recuperarTexto("txtApellido");
     let edad=recuperarInt("txtEdad");
     let estatura=recuperarFloat("txtEstatura");
-    
-}
-recuperarTexto=function(idComponente){
-    let componente=document.getElementById(idComponente);
-    let valorIngresado=componente.value;
-    return valorIngresado;
-}
-recuperarInt=function(idComponente){
-    let valorInt=recuperarTexto(idComponente);
-    valorInt=parseInt(valorInt)
-    return valorInt;
-}
-recuperarFloat=function(idComponente){
-    let valorFloat=recuperarTexto(idComponente);
-    valorFloat=parseFloat(valorFloat);
-    return valorFloat;
+    let mensaje="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("lblMensaje",mensaje);
+    mostrarImagen("imgPrueba","./imagenes/hola.gif")
+    mostrarTextoEnCaja("txtNombre","");
 }
