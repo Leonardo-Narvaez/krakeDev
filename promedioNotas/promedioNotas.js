9dc37bc7d1c8cad3bfd1152cd6dffbd1ba0faadd
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2025-09-26 23:30:04 -0500

    se crea los archivos para promedioNotas, se agrega cajas de texto y se emplea las funciones para recuperar los valores y calculara dicho promedio

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,8 @@
+calcularPromedioNotas=function(){
+    let n1=recuperarFloat("nota1");
+    let n2=recuperarFloat("nota2");
+    let n3=recuperarFloat("nota3");
+    let promedio=calcularPromedio(n1,n2,n3);
+    promedio=promedio.toFixed(2);
+    mostrarTexto("resultado",promedio);
+}
