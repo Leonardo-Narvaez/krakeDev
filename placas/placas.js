7fdf43e89a219d3639536544ec3747dd43a63bea
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2025-10-13 22:26:20 -0500

    se agrega funcion para obtener e ltipo de vehiculo y se cambia el estilo en css

--- placas/placas.js
+++ placas/placas.js
@@ -1,16 +1,21 @@
-validarPlaca=function(){
+validarPlaca = function () {
     let placa = recuperarTexto("placa");
     let erroresEstructura = validarEstructura(placa);
-    if (erroresEstructura === null){
+    if (erroresEstructura === null) {
         mostrarTexto("lblResultado", "Estructura Valida");
         let erroresProvincia = obtenerProvincia(placa);
-        if(erroresProvincia === null){
-            mostrarTexto("provincia","Provincia Incorrecta");
+        if (erroresProvincia === null) {
+            mostrarTexto("provincia", "Provincia Incorrecta");
         }
- 
+        let erroresVehiculo = obtenerTipoVehiculo(placa);
+        if (erroresVehiculo === null) {
+            mostrarTexto("vehiculo", "Tipo de vehiculo incorrecto")
+        }
 
-    }else{
+
+    } else {
         mostrarTexto("lblResultado", "Estructura Incorrecta");
         mostrarTexto("provincia", "");
+        mostrarTexto("vehiculo", "");
     }
 }
--- placas/serviciosPlacas.js
+++ placas/serviciosPlacas.js
@@ -59,100 +59,130 @@ obtenerProvincia = function (placa) {
     let caracter = placa.charAt(0);
     let errores = null;
     if (caracter == "A") {
-        mostrarTexto("provincia","Provincia: Azuay");
+        mostrarTexto("provincia", "Provincia: Azuay");
         errores = undefined;
     }
     if (caracter == "B") {
-        mostrarTexto("provincia","Provincia: Bolívar");
+        mostrarTexto("provincia", "Provincia: Bolívar");
         errores = undefined;
     }
     if (caracter == "U") {
-        mostrarTexto("provincia","Provincia: Cañar");
+        mostrarTexto("provincia", "Provincia: Cañar");
         errores = undefined;
     }
     if (caracter == "C") {
-        mostrarTexto("provincia","Provincia: Carchi");
+        mostrarTexto("provincia", "Provincia: Carchi");
         errores = undefined;
     }
     if (caracter == "X") {
-        mostrarTexto("provincia","Provincia: Cotopaxi");
+        mostrarTexto("provincia", "Provincia: Cotopaxi");
         errores = undefined;
     }
     if (caracter == "H") {
-        mostrarTexto("provincia","Provincia: Chimborazo");
+        mostrarTexto("provincia", "Provincia: Chimborazo");
         errores = undefined;
     }
     if (caracter == "O") {
-        mostrarTexto("provincia","Provincia: El Oro");
+        mostrarTexto("provincia", "Provincia: El Oro");
         errores = undefined;
     }
     if (caracter == "E") {
-        mostrarTexto("provincia","Provincia: Esmeraldas");
+        mostrarTexto("provincia", "Provincia: Esmeraldas");
         errores = undefined;
     }
     if (caracter == "W") {
-        mostrarTexto("provincia","Provincia: Galápagos");
+        mostrarTexto("provincia", "Provincia: Galápagos");
         errores = undefined;
     }
     if (caracter == "G") {
-        mostrarTexto("provincia","Provincia: Guayas");
+        mostrarTexto("provincia", "Provincia: Guayas");
         errores = undefined;
     }
     if (caracter == "I") {
-        mostrarTexto("provincia","Provincia: Imbabura");
+        mostrarTexto("provincia", "Provincia: Imbabura");
         errores = undefined;
     }
     if (caracter == "L") {
-        mostrarTexto("provincia","Provincia: Loja");
+        mostrarTexto("provincia", "Provincia: Loja");
         errores = undefined;
     }
     if (caracter == "R") {
-        mostrarTexto("provincia","Provincia: Los Ríos");
+        mostrarTexto("provincia", "Provincia: Los Ríos");
         errores = undefined;
     }
     if (caracter == "M") {
-        mostrarTexto("provincia","Provincia: Manabí");
+        mostrarTexto("provincia", "Provincia: Manabí");
         errores = undefined;
     }
     if (caracter == "V") {
-        mostrarTexto("provincia","Provincia: Morona Santiago");
+        mostrarTexto("provincia", "Provincia: Morona Santiago");
         errores = undefined;
     }
     if (caracter == "N") {
-        mostrarTexto("provincia","Provincia: Napo");
+        mostrarTexto("provincia", "Provincia: Napo");
         errores = undefined;
     }
     if (caracter == "S") {
-        mostrarTexto("provincia","Provincia: Pastaza");
+        mostrarTexto("provincia", "Provincia: Pastaza");
         errores = undefined;
     }
     if (caracter == "P") {
-        mostrarTexto("provincia","Provincia: Pichincha");
+        mostrarTexto("provincia", "Provincia: Pichincha");
         errores = undefined;
     }
     if (caracter == "K") {
-        mostrarTexto("provincia","Provincia: Sucumbíos");
+        mostrarTexto("provincia", "Provincia: Sucumbíos");
         errores = undefined;
     }
     if (caracter == "Q") {
-        mostrarTexto("provincia","Provincia: Orellana");
+        mostrarTexto("provincia", "Provincia: Orellana");
         errores = undefined;
     }
     if (caracter == "T") {
-        mostrarTexto("provincia","Provincia: Tungurahua");
+        mostrarTexto("provincia", "Provincia: Tungurahua");
         errores = undefined;
     }
     if (caracter == "Z") {
-        mostrarTexto("provincia","Provincia: Zamora Chinchipe");
+        mostrarTexto("provincia", "Provincia: Zamora Chinchipe");
         errores = undefined;
     }
     if (caracter == "Y") {
-        mostrarTexto("provincia","Provincia: Santa Elena");
+        mostrarTexto("provincia", "Provincia: Santa Elena");
         errores = undefined;
     }
-    if(caracter == "J"){
-        mostrarTexto("provincia","Provincia: Santo Domingo de los Tsáchilas");
+    if (caracter == "J") {
+        mostrarTexto("provincia", "Provincia: Santo Domingo de los Tsáchilas");
         errores = undefined;
     }
     return errores;
 }
+obtenerTipoVehiculo = function (placa) {
+    let caracter = placa.charAt(1);
+    let errores = null;
+    if (caracter == "A" || caracter == "Z") {
+        mostrarTexto("vehiculo", "Tipo: Comercial");
+        errores = undefined;
+    }
+    if (caracter == "E") {
+        mostrarTexto("vehiculo", "Tipo: Gubernamental");
+        errores = undefined;
+    }
+    if (caracter == "X") {
+        mostrarTexto("vehiculo", "Tipo: de Uso Oficial");
+        errores = undefined;
+    }
+    if (caracter == "S") {
+        mostrarTexto("vehiculo", "Tipo: Gobierno Provincial");
+        errores = undefined;
+    }
+    if (caracter == "M") {
+        mostrarTexto("vehiculo", "Tipo: Municipal");
+        errores = undefined;
+    }
+    if (caracter != "A" && caracter != "Z" && caracter != "E" && caracter != "X" && caracter != "S" && caracter != "M") {
+        mostrarTexto("vehiculo", "Tipo: Particular");
+        errores = undefined;
+    }
+    return errores;
+}
+
